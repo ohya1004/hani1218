84abfd7ab25c897aad0d749e2476a3c836ed2405
yy <yuyingg0921@gmail.com>
2016-12-19 23:15:28 +0800

    make it better

--- echobot/views.py
+++ echobot/views.py
@@ -1,3 +1,5 @@
+from django.shortcuts import render
+
 from django.conf import settings
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
 from django.views.decorators.csrf import csrf_exempt
@@ -5,19 +7,34 @@ from django.views.decorators.csrf import csrf_exempt
 from linebot import LineBotApi, WebhookParser
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
-import urllib
+
 from urllib.request import urlopen
-import requests
-import re
+from xml.etree.ElementTree import parse
+
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
 
+def get_weather(location):
+    url = 'http://opendata.cwb.gov.tw/opendataapi?dataid=F-C0032-001&authorizationkey=CWB-15472AB4-58F6-430C-AF81-7B4BCFC16BAE'
+    response = urlopen(url)
+    tree = parse(response)
+    root = tree.getroot();
+    NS = '{urn:cwb:gov:tw:cwbcommon:0.1}'
+
+    for item in root.iterfind(".//{NS}location".format(NS = NS)):
+        if location in item[0].text:
+            weather = item.find(".//{NS}parameterName".format(NS = NS))
+            return item[0].text + weather.text
+    return "Can't find " + location
 
 @csrf_exempt
 def callback(request):
     if request.method == 'POST':
         signature = request.META['HTTP_X_LINE_SIGNATURE']
         body = request.body.decode('utf-8')
+        location_list = ["臺北", "新北", "桃園", "臺中", "臺南", "高雄", "基隆",\
+                        "新竹", "苗栗", "彰化", "南投", "雲林", "嘉義", "屏東",\
+                        "宜蘭", "花蓮", "台東", "澎湖", "金門", "連江"]
 
         try:
             events = parser.parse(body, signature)
@@ -25,31 +42,34 @@ def callback(request):
             return HttpResponseForbidden()
         except LineBotApiError:
             return HttpResponseBadRequest()
-        weather = "天氣1"
-        try:
-            weather = "天氣2"
-            k = "http://opendata.cwb.gov.tw/opendataapi?dataid=F-C0032-001&authorizationkey=CWB-E2BF5AB5-CB0D-4434-ABD8-1A1C7AF82F3D"
-            c = urlopen(k)
-            weather = "天氣3"
-            c1 = c.split('<locationName>臺北市</locationName>')
-            c2 = c1[1].split('<parameterName>')
-            c3 = c2[1].split('</parameterName>')
-            weather = "天氣4"
-        except:
-            pass
+
         for event in events:
             if isinstance(event, MessageEvent):
                 if isinstance(event.message, TextMessage):
-                    if "臺南" in event.message.text :
-                        line_bot_api.reply_message(
-                            event.reply_token,
-                            TextSendMessage(text=weather)
-                        )
+                    if "天氣" in event.message.text:
+                        output = get_weather("臺南")
+                        for location in location_list:
+                            if location in event.message.text:
+                                if location == "新竹":
+                                    if "新竹縣" in event.message.text:
+                                        output = get_weather("新竹縣")
+                                    else:
+                                        output = get_weather("新竹市")
+                                elif location == "嘉義":
+                                    if "嘉義縣" in event.message.text:
+                                        output = get_weather("嘉義縣")
+                                    else:
+                                        output = get_weather("嘉義市")
+                                else:
+                                    output = get_weather(location)
+                                break;
                     else:
-                        line_bot_api.reply_message(
-                            event.reply_token,
-                            TextSendMessage(text="其他")
-                        )
+                        output = event.message.text
+
+                    line_bot_api.reply_message(
+                        event.reply_token,
+                        TextSendMessage(text = output)
+                    )
 
         return HttpResponse()
     else:
