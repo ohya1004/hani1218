3bb7c891c29d09b344c6cb6b6054d99b5caf6728
yy <yuyingg0921@gmail.com>
2016-12-19 21:05:07 +0800

    make it better

--- echobot/views.py
+++ echobot/views.py
@@ -5,14 +5,12 @@ from django.views.decorators.csrf import csrf_exempt
 from linebot import LineBotApi, WebhookParser
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
-import urllib
 
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
 
 
 @csrf_exempt
-	
 def callback(request):
     if request.method == 'POST':
         signature = request.META['HTTP_X_LINE_SIGNATURE']
@@ -31,18 +29,14 @@ def callback(request):
                     if "臺南" in event.message.text :
                         line_bot_api.reply_message(
                             event.reply_token,
-                            TextSendMessage(text="有臺南")
+                            TextSendMessage(text="有:臺南")
                         )
-                    else if "臺北" in event.message.text:
+                    else:
                         line_bot_api.reply_message(
                             event.reply_token,
                             TextSendMessage(text="其他")
                         )
-                    else if "新北" in event.message.text:
-                        line_bot_api.reply_message(
-                            event.reply_token,
-                            TextSendMessage(text="其他")
-                        )
+
         return HttpResponse()
     else:
         return HttpResponseBadRequest()
